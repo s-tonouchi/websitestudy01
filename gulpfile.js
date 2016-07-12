15aebb9c179947455adf2731df77e887c4337fab
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-12 15:00:10 +1200

    deleted all ./

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,13 @@ gulp.task('default', ['connect', 'watch']);
 gulp.task('connect', () => {
   return connect.server({
     port: 8080,
-    root: './build',
+    root: 'build',
     livereload: true
   });
 });
 
 gulp.task('reload', () => {
-  return gulp.src('./build/**/*')
+  return gulp.src('build/**/*')
              .pipe(connect.reload());
 });
 
@@ -32,18 +32,18 @@ gulp.task('watch', () => {
 
 
 gulp.task('copy_html', () => {
-  gulp.src('./src/html/**/*.html')
-      .pipe(gulp.dest('./build'));
+  gulp.src('src/html/**/*.html')
+      .pipe(gulp.dest('build'));
 });
 
 gulp.task('build_scss', () => {
-  return gulp.src('./src/scss/**/*.scss')
+  return gulp.src('src/scss/**/*.scss')
              .pipe(sass().on('error', sass.logError))
-             .pipe(gulp.dest('./build/css'));
+             .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('build_ts', () => {
-  return gulp.src('./src/ts/**/*.ts')
+  return gulp.src('src/ts/**/*.ts')
              .pipe(ts({
                noImplicitAny: true,
                out: 'output.js'
@@ -52,8 +52,8 @@ gulp.task('build_ts', () => {
 });
 
 gulp.task('copy_images', () => {
-  gulp.src('./src/images/**/*')
-      .pipe(gulp.dest('./build/images'));
+  gulp.src('src/images/**/*')
+      .pipe(gulp.dest('build/images'));
 });
 
 gulp.task('build', ['clean'], () => {
