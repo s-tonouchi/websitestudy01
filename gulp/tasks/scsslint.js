4aa2bb81178db5916c3416c88531fbec48c20e4d
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-15 22:10:06 +1200

    added scsslint task

---
+++ gulp/tasks/scsslint.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sassLint  = require('gulp-sass-lint');
+
+var scsslintFunction = function(cb) {
+  gulp.src('src/scss/**/*.scss')
+      .pipe(sassLint())
+      .pipe(sassLint.format())
+      .pipe(sassLint.failOnError());
+};
+
+gulp.task('scsslint', scsslintFunction);
+module.exports = scsslintFunction; 
