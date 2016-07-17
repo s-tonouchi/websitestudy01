7050ea02af48a1fcdc0ed159552ce166031b4af6
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-17 16:08:54 +1200

    added htmllint task refering https://www.npmjs.com/package/gulp-htmllint

---
+++ gulp/tasks/htmllint.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var htmllint = require('gulp-htmllint');
+var gutil = require('gulp-util');
+
+var htmllintFunction = function(cb) {
+  return gulp.src('src/html/**/*.html')
+             .pipe(htmllint({}, htmllintReporter));
+}
+
+function htmllintReporter(filepath, issues) {
+  if (issues.length > 0) {
+    issues.forEach(function (issue) {
+      gutil.log(gutil.colors.cyan('[gulp-htmllint]' + 
+                gutil.colors.red(filepath + ' [' + issue.line + ',' + issue.column + ']: ') +
+                gutil.colors.cyan('(' + issue.code + ') ' + 
+                issue.msg)))});
+    process.exitCode = 1;
+  };
+}
+
+gulp.task('htmllint', htmllintFunction);
+		
