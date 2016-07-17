56cf1863d1c6444f6751ef556109e01b3b9246ac
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-17 22:21:17 +1200

    added log functions

---
+++ gulp/util/warnlog.js
@@ -0,0 +1,11 @@
+var gutil = require('gulp-util');
+
+var warn = function(module, file, message) {
+  gutil.log(gutil.colors.cyan('[' + module + '] ') +
+            gutil.colors.yellow('W ') +
+            gutil.colors.yellow(file.path + ' ') +
+            gutil.colors.yellow(message));
+}
+
+module.exports = warn;
+
