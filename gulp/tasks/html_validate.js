a2029cb3f4b2592594d8ade97a5fd83f55452732
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-17 18:06:49 +1200

    summarize the resul

--- gulp/tasks/html_validate.js
+++ gulp/tasks/html_validate.js
@@ -1,10 +1,21 @@
-var gulp = require('gulp');
-var htmlv  = require('gulp-html-validator');
+var gulp      = require('gulp');
+var gutil     = require('gulp-util');
+var htmlv     = require('gulp-html-validator');
+var intercept = require('gulp-intercept');
 
 var htmlValidateFunction = function(cb) {
   gulp.src('src/html/**/*.html')
-      .pipe(htmlv({format: 'html'}))
-      .pipe(gulp.dest('out/html_validate'));
+      .pipe(htmlv({format: 'json'}))
+      .pipe(intercept(function(file){
+          json = JSON.parse(file.contents.toString());
+          json.messages.forEach(function (message) {
+            gutil.log(
+              gutil.colors.cyan('[gulp-html-validator] ') +
+              gutil.colors.red(message.type)             +
+              gutil.colors.white(' [' + message.lastLine + ':' + message.firstColumn + '] ' + message.message)
+            )
+          });
+      }));
 };
 
 gulp.task('html_validate', htmlValidateFunction);
