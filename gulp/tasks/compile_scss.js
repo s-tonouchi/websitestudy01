ca1177b5018c30dcd441078d50adf5060868cbe2
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-18 21:59:36 +1200

    renamed

--- gulp/tasks/compile_scss.js
+++ gulp/tasks/compile_scss.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 
-var buildScssFunction = function(cb) {
+var compileScssFunction = function(cb) {
   return gulp.src('src/scss/**/*.scss')
              .pipe(sass().on('error', sass.logError))
              .pipe(gulp.dest('build/css'));
 };
 
-gulp.task('build_scss', buildScssFunction);
-module.exports = buildScssFunction; 
+gulp.task('compile_scss', compileScssFunction);
+module.exports = compileScssFunction; 
