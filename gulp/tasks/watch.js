e4e02edbb82402cc9788203d007422aeafe54023
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-18 21:59:12 +1200

    changed sequence for scss

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,10 +5,10 @@ var sequence = require('gulp-watch-sequence');
 gulp.task('watch', () => {
   var queue = sequence(300);
 
-  watch('src/html/**/*.html', {}, queue.getHandler('htmllint', 'html_validate', 'copy_html'));
-  watch('src/scss/**/*.scss', {}, queue.getHandler('build_scss'));
-  watch('src/ts/**/*.ts'    , {}, queue.getHandler('tslint', 'compile_ts'));
-  watch('src/images/**/*'   , {}, queue.getHandler('copy_images'));
+  watch('src/html/**/*.html', {}, queue.getHandler('htmllint'   , 'html_validate', 'copy_html'));
+  watch('src/scss/**/*.scss', {}, queue.getHandler('scsslint'   , 'compile_scss'              ));
+  watch('src/ts/**/*.ts'    , {}, queue.getHandler('tslint'     , 'compile_ts'                ));
+  watch('src/images/**/*'   , {}, queue.getHandler('copy_images'                              ));
 
   watch('build/**/*', {}, queue.getHandler('reload'));
 });
