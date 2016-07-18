0523290074f114fd9e99e454206412c0c57aab14
Seiji Tonouchi <s.tonouchi@gmail.com>
2016-07-18 15:25:44 +1200

    use log

--- gulp/tasks/copy_images.js
+++ gulp/tasks/copy_images.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var info = require('../util/info');
+var intercept = require('gulp-intercept');
 
 var copyImagesFunction = function(cb) {
   gulp.src('src/images/**/*')
+      .pipe(intercept(function(file){
+         info('copy_images', file.path, 'copying');
+       }))
       .pipe(gulp.dest('build/images'));
 };
 
